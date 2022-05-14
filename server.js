feee3af06b599f1ce22852a41cb77c55f1d8320a
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-05-14 12:58:08 -0400

    added port for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 app.use(express.static('public'));
 
